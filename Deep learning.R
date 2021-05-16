bd5d6990be12441a15cdd4b66ba2e3779987b41d
Colleen Sellwood <C.Sellwood@newcastle.ac.uk>
2021-05-16 14:19:46 +0100

    Download images of green shield bugs

--- Deep learning.R
+++ Deep learning.R
@@ -1,5 +1,12 @@
 #assignment part 2 - deep learning models
+
 library(rinat)
 library(sf)
 source("download_images.R") #for downloading image from CS
 gb_ll <- readRDS("gb_simple.RDS") #for mapping
+
+#get some green shield bug images
+green_recs <-  get_inat_obs(taxon_name  = "Palomena prasina",
+                                bounds = gb_ll,
+                                quality = "research",
+                                maxresults = 800)
